e54916d8de4e9d55f41a9f3eaa64d561b4f2e585
wenhuili-vivid <15991890150@163.com>
2022-02-11 15:55:22 +0800

    add ToDoItem details

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import DeleteBtn from './DeleteBtn';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class ToDoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFinished: false,
+      description: '',
+    };
+  }
+
+  handleChange = () => {
+    const { isFinished } = this.state;
+    this.setState({
+      isFinished: !isFinished,
+    });
+  };
+
+  handleInput = (e) => {
+    this.setState({
+      description: e.target.value,
+    });
+  };
+
   render() {
+    const { isFinished, description } = this.state;
     return (
       <li>
-        <input type="checkbox" checked />
-        <span>这是一个item</span>
+        <input type="checkbox" checked={isFinished} onChange={this.handleChange} />
+        <input type="text" placeholder="请输入事项内容" value={description} onChange={this.handleInput} disabled={isFinished} />
         <DeleteBtn />
       </li>
     );

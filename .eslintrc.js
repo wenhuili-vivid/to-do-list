ad114025922120415be2d7aa20376796bcde9a1a
wenhuili-vivid <15991890150@163.com>
2022-02-14 12:55:59 +0800

    install storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,29 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ["plugin:react/recommended", "airbnb", "plugin:storybook/recommended"],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/destructuring-assignment': ['error', 'always', { ignoreClassFields: true }],
-    'react/prop-types': ['error', { ignore: [], customValidators: [], skipUndeclared: true }],
-  },
+    'react/jsx-filename-extension': [1, {
+      extensions: ['.js', '.jsx']
+    }],
+    'react/destructuring-assignment': ['error', 'always', {
+      ignoreClassFields: true
+    }],
+    'react/prop-types': ['error', {
+      ignore: [],
+      customValidators: [],
+      skipUndeclared: true
+    }]
+  }
 };
